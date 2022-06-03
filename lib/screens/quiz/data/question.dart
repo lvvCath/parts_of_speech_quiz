cc81b53c36a1e7ef145cbf6981cc9a742f9e6a76
JLawrence1228 <jayel1228@gmail.com>
2022-06-03 11:33:16 +0800

    update question.dart
    questions for noun:
    - easy
    - medium
    - hard

--- lib/screens/quiz/data/question.dart
+++ lib/screens/quiz/data/question.dart
@@ -72,35 +72,985 @@ List<QuestionModel> questionAllInOne = [
 
 List<QuestionModel> questionPractice = [];
 
-List<QuestionModel> questionNounEasy = [];
+List<QuestionModel> questionNounEasy = [
+  QuestionModel(
+      "Which singular noun requires that you add an -es to the end to make it plural?",
+      {
+        "keyboard": false,
+        "proof": false,
+        "fox": true,
+        "Sister-in-law": false,
+      }),
+  QuestionModel("Which of the following plural nouns is spelled incorrectly?", {
+    "brothers-in-law": false,
+    "donkies": true,
+    "knives": false,
+    "masses": false,
+  }),
+  QuestionModel("What is a noun?", {
+    "Action words": false,
+    "Describing words": false,
+    "Person, place, or thing": true,
+    "Color Words": false,
+  }),
+  QuestionModel(
+      "Heroes never lay down their words. Which word is a plural noun?", {
+    "never": false,
+    "heroes": true,
+    "lay down": false,
+    "words": false,
+  }),
+  QuestionModel("Which is not a noun?", {
+    "Dog": false,
+    "School": false,
+    "Running": true,
+    "Ms. Taylor": false,
+  }),
+  QuestionModel(
+      "Which word is a noun in the sentence? “Can you help me with my homework please?”",
+      {
+        "please": false,
+        "help": false,
+        "my": false,
+        "homework": true,
+      }),
+  QuestionModel("Which word below is a proper noun?", {
+    "city": false,
+    "business": false,
+    "Kailua-Kona": true,
+    "school": false,
+  }),
+  QuestionModel("The plural of dish is ___", {
+    "dishese": false,
+    "dishs": false,
+    "dishes": true,
+    "dish's": false,
+  }),
+  QuestionModel("What does SINGULAR mean?", {
+    "two": false,
+    "one": true,
+    "zero": false,
+    "more than one": false,
+  }),
+  QuestionModel("10.How can you tell if a noun is a proper noun?", {
+    "It starts with a capital letter.": true,
+    "It starts with a lowercase letter.": false,
+    "It has an apostrophe.": false,
+    "It tells you.": false,
+  }),
+];
 
-List<QuestionModel> questionNounMedium = [];
+List<QuestionModel> questionNounMedium = [
+  QuestionModel(
+      "How many nouns are there in this sentence? “My brother gave me a book for my birthday?”",
+      {
+        "3": true,
+        "2": false,
+        "4": false,
+        "1": false,
+      }),
+  QuestionModel(
+      "How many nouns are there in this sentence? “They are forecasting rain later.”",
+      {
+        "rain": true,
+        "later": false,
+        "forecasting": false,
+        "they": false,
+      }),
+  QuestionModel(
+      "Which word is used as a common noun? “My cousin and I saw the posters on the bulletin board at the grocery store announcing the arrival of the circus in our town.”",
+      {
+        "posters": false,
+        "announcing": false,
+        "saw": false,
+        "cousin": true,
+      }),
+  QuestionModel(
+      "The boys will go to school on Wednesday. Which word is a proper noun?", {
+    "noun": false,
+    "Wednesday": true,
+    "boys": false,
+    "school": false,
+  }),
+  QuestionModel(
+      "What are the common nouns in this sentence? “The rabbit was always late and Alice followed him into his hole.”",
+      {
+        "what, him, was": false,
+        "rabbit, hole": true,
+        "Alice, rabbit, hole": false,
+        "late, into, his": false,
+      }),
+  QuestionModel("Which word is an example of a proper noun?", {
+    "city": false,
+    "New York": true,
+    "state": false,
+    "Country": false,
+  }),
+  QuestionModel(
+      "Which noun is supposed to be capitalized because it is a proper noun?", {
+    "cheshire cat": false,
+    "alice": false,
+    "wonderland": false,
+    "all of the above": true,
+  }),
+  QuestionModel(
+      "The crowd was on the main street. Which word is a collective noun?", {
+    "crowd": true,
+    "main": false,
+    "on": false,
+    "street": false,
+  }),
+  QuestionModel("Which word is an abstract noun?", {
+    "state": false,
+    "peace": true,
+    "need": false,
+    "Our": false,
+  }),
+  QuestionModel(
+      "Beauty does not need any ornaments. Which word is an uncountable noun?",
+      {
+        "need": false,
+        "beauty": true,
+        "any ornaments": false,
+        "": false,
+      }),
+];
 
-List<QuestionModel> questionNounHard = [];
+List<QuestionModel> questionNounHard = [
+  QuestionModel(
+      "This nouns are used to name a GENERAL type of person, place, or thing.",
+      {
+        "Compound Nouns": false,
+        "Proper Nouns": false,
+        "Common Nouns": true,
+        "Mass Nouns": false,
+      }),
+  QuestionModel(
+      "This nouns are used to name a SPECIFIC person, place, or thing. This noun begins with a capital letter.",
+      {
+        "Concrete Noun": false,
+        "Compound Noun": false,
+        "Proper Noun": true,
+        "Abstract Noun": false,
+      }),
+  QuestionModel(
+      "What type of nouns are two or more words that create a noun. This noun are sometime one word or words joined by a hyphen.",
+      {
+        "Proper Noun": false,
+        "Countable Noun": false,
+        "Abstract Noun": false,
+        "Compound Noun": true,
+      }),
+  QuestionModel(
+      "This is a noun that CAN be counted. They have a singular and plural form and can be used with a number.",
+      {
+        "Compound Noun": false,
+        "Collective Noun": false,
+        "Mass Noun": false,
+        "Countable Noun": true,
+      }),
+  QuestionModel("This nouns are nouns that CANNOT be counted.", {
+    "Abstract Noun": false,
+    "Compound Noun": false,
+    "Countable Noun": false,
+    "Mass Noun": true,
+  }),
+  QuestionModel(
+      "This nouns are words that refer to a set of group of people, animal, or things.",
+      {
+        "Collective Noun": true,
+        "Compound Noun": false,
+        "Abstract Noun": false,
+        "Proper Noun": false,
+      }),
+  QuestionModel(
+      "This nouns are nouns which refer to people and things that exist physically and that at least one of the senses can detect.",
+      {
+        "Mass Noun": false,
+        "Common Noun": false,
+        "Concrete Noun": true,
+        "Abstract Noun": false,
+      }),
+  QuestionModel(
+      "This nouns are nouns that have no physical existence and are not concrete. They refer to ideas, emotions or concepts so you CANNOT see, touch, hear, smell, or taste something.",
+      {
+        "Proper Noun": false,
+        "Countable Noun": false,
+        "Common Noun": false,
+        "Abstract Noun": true,
+      }),
+  QuestionModel(
+      "Honesty is the best policy. The word ‘honesty’ is what type of nouns?", {
+    "Common Noun": false,
+    "Proper Noun": false,
+    "Abstract Noun": true,
+    "Countable Noun": false,
+  }),
+  QuestionModel(
+      "My family lives in that house. The word ‘family’ is what type of nouns?",
+      {
+        "Count Noun": false,
+        "Common Noun": false,
+        "Concrete Noun": false,
+        "Collective Noun": true,
+      }),
+];
 
-List<QuestionModel> questionPronounEasy = [];
+List<QuestionModel> questionPronounEasy = [
+  QuestionModel("", {
+    "": false,
+    "": true,
+    "": false,
+    "": false,
+  }),
+  QuestionModel("", {
+    "": false,
+    "": true,
+    "": false,
+    "": false,
+  }),
+  QuestionModel("", {
+    "": false,
+    "": true,
+    "": false,
+    "": false,
+  }),
+  QuestionModel("", {
+    "": false,
+    "": true,
+    "": false,
+    "": false,
+  }),
+  QuestionModel("", {
+    "": false,
+    "": true,
+    "": false,
+    "": false,
+  }),
+  QuestionModel("", {
+    "": false,
+    "": true,
+    "": false,
+    "": false,
+  }),
+  QuestionModel("", {
+    "": false,
+    "": true,
+    "": false,
+    "": false,
+  }),
+  QuestionModel("", {
+    "": false,
+    "": true,
+    "": false,
+    "": false,
+  }),
+  QuestionModel("", {
+    "": false,
+    "": true,
+    "": false,
+    "": false,
+  }),
+  QuestionModel("", {
+    "": false,
+    "": true,
+    "": false,
+    "": false,
+  }),
+];
 
-List<QuestionModel> questionPronounMedium = [];
+List<QuestionModel> questionPronounMedium = [
+  QuestionModel("", {
+    "": false,
+    "": true,
+    "": false,
+    "": false,
+  }),
+  QuestionModel("", {
+    "": false,
+    "": true,
+    "": false,
+    "": false,
+  }),
+  QuestionModel("", {
+    "": false,
+    "": true,
+    "": false,
+    "": false,
+  }),
+  QuestionModel("", {
+    "": false,
+    "": true,
+    "": false,
+    "": false,
+  }),
+  QuestionModel("", {
+    "": false,
+    "": true,
+    "": false,
+    "": false,
+  }),
+  QuestionModel("", {
+    "": false,
+    "": true,
+    "": false,
+    "": false,
+  }),
+  QuestionModel("", {
+    "": false,
+    "": true,
+    "": false,
+    "": false,
+  }),
+  QuestionModel("", {
+    "": false,
+    "": true,
+    "": false,
+    "": false,
+  }),
+  QuestionModel("", {
+    "": false,
+    "": true,
+    "": false,
+    "": false,
+  }),
+  QuestionModel("", {
+    "": false,
+    "": true,
+    "": false,
+    "": false,
+  }),
+];
 
-List<QuestionModel> questionPronounHard = [];
+List<QuestionModel> questionPronounHard = [
+  QuestionModel("", {
+    "": false,
+    "": true,
+    "": false,
+    "": false,
+  }),
+  QuestionModel("", {
+    "": false,
+    "": true,
+    "": false,
+    "": false,
+  }),
+  QuestionModel("", {
+    "": false,
+    "": true,
+    "": false,
+    "": false,
+  }),
+  QuestionModel("", {
+    "": false,
+    "": true,
+    "": false,
+    "": false,
+  }),
+  QuestionModel("", {
+    "": false,
+    "": true,
+    "": false,
+    "": false,
+  }),
+  QuestionModel("", {
+    "": false,
+    "": true,
+    "": false,
+    "": false,
+  }),
+  QuestionModel("", {
+    "": false,
+    "": true,
+    "": false,
+    "": false,
+  }),
+  QuestionModel("", {
+    "": false,
+    "": true,
+    "": false,
+    "": false,
+  }),
+  QuestionModel("", {
+    "": false,
+    "": true,
+    "": false,
+    "": false,
+  }),
+  QuestionModel("", {
+    "": false,
+    "": true,
+    "": false,
+    "": false,
+  }),
+];
 
-List<QuestionModel> questionVerbEasy = [];
+List<QuestionModel> questionVerbEasy = [
+  QuestionModel("", {
+    "": false,
+    "": true,
+    "": false,
+    "": false,
+  }),
+  QuestionModel("", {
+    "": false,
+    "": true,
+    "": false,
+    "": false,
+  }),
+  QuestionModel("", {
+    "": false,
+    "": true,
+    "": false,
+    "": false,
+  }),
+  QuestionModel("", {
+    "": false,
+    "": true,
+    "": false,
+    "": false,
+  }),
+  QuestionModel("", {
+    "": false,
+    "": true,
+    "": false,
+    "": false,
+  }),
+  QuestionModel("", {
+    "": false,
+    "": true,
+    "": false,
+    "": false,
+  }),
+  QuestionModel("", {
+    "": false,
+    "": true,
+    "": false,
+    "": false,
+  }),
+  QuestionModel("", {
+    "": false,
+    "": true,
+    "": false,
+    "": false,
+  }),
+  QuestionModel("", {
+    "": false,
+    "": true,
+    "": false,
+    "": false,
+  }),
+  QuestionModel("", {
+    "": false,
+    "": true,
+    "": false,
+    "": false,
+  }),
+];
 
-List<QuestionModel> questionVerbMedium = [];
+List<QuestionModel> questionVerbMedium = [
+  QuestionModel("", {
+    "": false,
+    "": true,
+    "": false,
+    "": false,
+  }),
+  QuestionModel("", {
+    "": false,
+    "": true,
+    "": false,
+    "": false,
+  }),
+  QuestionModel("", {
+    "": false,
+    "": true,
+    "": false,
+    "": false,
+  }),
+  QuestionModel("", {
+    "": false,
+    "": true,
+    "": false,
+    "": false,
+  }),
+  QuestionModel("", {
+    "": false,
+    "": true,
+    "": false,
+    "": false,
+  }),
+  QuestionModel("", {
+    "": false,
+    "": true,
+    "": false,
+    "": false,
+  }),
+  QuestionModel("", {
+    "": false,
+    "": true,
+    "": false,
+    "": false,
+  }),
+  QuestionModel("", {
+    "": false,
+    "": true,
+    "": false,
+    "": false,
+  }),
+  QuestionModel("", {
+    "": false,
+    "": true,
+    "": false,
+    "": false,
+  }),
+  QuestionModel("", {
+    "": false,
+    "": true,
+    "": false,
+    "": false,
+  }),
+];
 
-List<QuestionModel> questionVerbHard = [];
+List<QuestionModel> questionVerbHard = [
+  QuestionModel("", {
+    "": false,
+    "": true,
+    "": false,
+    "": false,
+  }),
+  QuestionModel("", {
+    "": false,
+    "": true,
+    "": false,
+    "": false,
+  }),
+  QuestionModel("", {
+    "": false,
+    "": true,
+    "": false,
+    "": false,
+  }),
+  QuestionModel("", {
+    "": false,
+    "": true,
+    "": false,
+    "": false,
+  }),
+  QuestionModel("", {
+    "": false,
+    "": true,
+    "": false,
+    "": false,
+  }),
+  QuestionModel("", {
+    "": false,
+    "": true,
+    "": false,
+    "": false,
+  }),
+  QuestionModel("", {
+    "": false,
+    "": true,
+    "": false,
+    "": false,
+  }),
+  QuestionModel("", {
+    "": false,
+    "": true,
+    "": false,
+    "": false,
+  }),
+  QuestionModel("", {
+    "": false,
+    "": true,
+    "": false,
+    "": false,
+  }),
+  QuestionModel("", {
+    "": false,
+    "": true,
+    "": false,
+    "": false,
+  }),
+];
 
-List<QuestionModel> questionAdverbEasy = [];
+List<QuestionModel> questionAdverbEasy = [
+  QuestionModel("", {
+    "": false,
+    "": true,
+    "": false,
+    "": false,
+  }),
+  QuestionModel("", {
+    "": false,
+    "": true,
+    "": false,
+    "": false,
+  }),
+  QuestionModel("", {
+    "": false,
+    "": true,
+    "": false,
+    "": false,
+  }),
+  QuestionModel("", {
+    "": false,
+    "": true,
+    "": false,
+    "": false,
+  }),
+  QuestionModel("", {
+    "": false,
+    "": true,
+    "": false,
+    "": false,
+  }),
+  QuestionModel("", {
+    "": false,
+    "": true,
+    "": false,
+    "": false,
+  }),
+  QuestionModel("", {
+    "": false,
+    "": true,
+    "": false,
+    "": false,
+  }),
+  QuestionModel("", {
+    "": false,
+    "": true,
+    "": false,
+    "": false,
+  }),
+  QuestionModel("", {
+    "": false,
+    "": true,
+    "": false,
+    "": false,
+  }),
+  QuestionModel("", {
+    "": false,
+    "": true,
+    "": false,
+    "": false,
+  }),
+];
 
-List<QuestionModel> questionAdverbMedium = [];
+List<QuestionModel> questionAdverbMedium = [
+  QuestionModel("", {
+    "": false,
+    "": true,
+    "": false,
+    "": false,
+  }),
+  QuestionModel("", {
+    "": false,
+    "": true,
+    "": false,
+    "": false,
+  }),
+  QuestionModel("", {
+    "": false,
+    "": true,
+    "": false,
+    "": false,
+  }),
+  QuestionModel("", {
+    "": false,
+    "": true,
+    "": false,
+    "": false,
+  }),
+  QuestionModel("", {
+    "": false,
+    "": true,
+    "": false,
+    "": false,
+  }),
+  QuestionModel("", {
+    "": false,
+    "": true,
+    "": false,
+    "": false,
+  }),
+  QuestionModel("", {
+    "": false,
+    "": true,
+    "": false,
+    "": false,
+  }),
+  QuestionModel("", {
+    "": false,
+    "": true,
+    "": false,
+    "": false,
+  }),
+  QuestionModel("", {
+    "": false,
+    "": true,
+    "": false,
+    "": false,
+  }),
+  QuestionModel("", {
+    "": false,
+    "": true,
+    "": false,
+    "": false,
+  }),
+];
 
-List<QuestionModel> questionAdverbHard = [];
+List<QuestionModel> questionAdverbHard = [
+  QuestionModel("", {
+    "": false,
+    "": true,
+    "": false,
+    "": false,
+  }),
+  QuestionModel("", {
+    "": false,
+    "": true,
+    "": false,
+    "": false,
+  }),
+  QuestionModel("", {
+    "": false,
+    "": true,
+    "": false,
+    "": false,
+  }),
+  QuestionModel("", {
+    "": false,
+    "": true,
+    "": false,
+    "": false,
+  }),
+  QuestionModel("", {
+    "": false,
+    "": true,
+    "": false,
+    "": false,
+  }),
+  QuestionModel("", {
+    "": false,
+    "": true,
+    "": false,
+    "": false,
+  }),
+  QuestionModel("", {
+    "": false,
+    "": true,
+    "": false,
+    "": false,
+  }),
+  QuestionModel("", {
+    "": false,
+    "": true,
+    "": false,
+    "": false,
+  }),
+  QuestionModel("", {
+    "": false,
+    "": true,
+    "": false,
+    "": false,
+  }),
+  QuestionModel("", {
+    "": false,
+    "": true,
+    "": false,
+    "": false,
+  }),
+];
 
-List<QuestionModel> questionAdjectiveEasy = [];
+List<QuestionModel> questionAdjectiveEasy = [
+  QuestionModel("", {
+    "": false,
+    "": true,
+    "": false,
+    "": false,
+  }),
+  QuestionModel("", {
+    "": false,
+    "": true,
+    "": false,
+    "": false,
+  }),
+  QuestionModel("", {
+    "": false,
+    "": true,
+    "": false,
+    "": false,
+  }),
+  QuestionModel("", {
+    "": false,
+    "": true,
+    "": false,
+    "": false,
+  }),
+  QuestionModel("", {
+    "": false,
+    "": true,
+    "": false,
+    "": false,
+  }),
+  QuestionModel("", {
+    "": false,
+    "": true,
+    "": false,
+    "": false,
+  }),
+  QuestionModel("", {
+    "": false,
+    "": true,
+    "": false,
+    "": false,
+  }),
+  QuestionModel("", {
+    "": false,
+    "": true,
+    "": false,
+    "": false,
+  }),
+  QuestionModel("", {
+    "": false,
+    "": true,
+    "": false,
+    "": false,
+  }),
+  QuestionModel("", {
+    "": false,
+    "": true,
+    "": false,
+    "": false,
+  }),
+];
 
-List<QuestionModel> questionAdjectiveMedium = [];
+List<QuestionModel> questionAdjectiveMedium = [
+  QuestionModel("", {
+    "": false,
+    "": true,
+    "": false,
+    "": false,
+  }),
+  QuestionModel("", {
+    "": false,
+    "": true,
+    "": false,
+    "": false,
+  }),
+  QuestionModel("", {
+    "": false,
+    "": true,
+    "": false,
+    "": false,
+  }),
+  QuestionModel("", {
+    "": false,
+    "": true,
+    "": false,
+    "": false,
+  }),
+  QuestionModel("", {
+    "": false,
+    "": true,
+    "": false,
+    "": false,
+  }),
+  QuestionModel("", {
+    "": false,
+    "": true,
+    "": false,
+    "": false,
+  }),
+  QuestionModel("", {
+    "": false,
+    "": true,
+    "": false,
+    "": false,
+  }),
+  QuestionModel("", {
+    "": false,
+    "": true,
+    "": false,
+    "": false,
+  }),
+  QuestionModel("", {
+    "": false,
+    "": true,
+    "": false,
+    "": false,
+  }),
+  QuestionModel("", {
+    "": false,
+    "": true,
+    "": false,
+    "": false,
+  }),
+];
 
-List<QuestionModel> questionAdjectiveHard = [];
+List<QuestionModel> questionAdjectiveHard = [
+  QuestionModel("", {
+    "": false,
+    "": true,
+    "": false,
+    "": false,
+  }),
+  QuestionModel("", {
+    "": false,
+    "": true,
+    "": false,
+    "": false,
+  }),
+  QuestionModel("", {
+    "": false,
+    "": true,
+    "": false,
+    "": false,
+  }),
+  QuestionModel("", {
+    "": false,
+    "": true,
+    "": false,
+    "": false,
+  }),
+  QuestionModel("", {
+    "": false,
+    "": true,
+    "": false,
+    "": false,
+  }),
+  QuestionModel("", {
+    "": false,
+    "": true,
+    "": false,
+    "": false,
+  }),
+  QuestionModel("", {
+    "": false,
+    "": true,
+    "": false,
+    "": false,
+  }),
+  QuestionModel("", {
+    "": false,
+    "": true,
+    "": false,
+    "": false,
+  }),
+  QuestionModel("", {
+    "": false,
+    "": true,
+    "": false,
+    "": false,
+  }),
+  QuestionModel("", {
+    "": false,
+    "": true,
+    "": false,
+    "": false,
+  }),
+];
 
 List<QuestionModel> questionConjuctionEasy = [];
 

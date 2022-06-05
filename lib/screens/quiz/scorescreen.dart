1d049053db95d102d13569055bd4c2037975fcd0
JLawrence1228 <jayel1228@gmail.com>
2022-06-05 21:26:37 +0800

    layout scorescreen.dart

--- lib/screens/quiz/scorescreen.dart
+++ lib/screens/quiz/scorescreen.dart
@@ -1,7 +1,13 @@
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:parts_of_speech_quiz/constants.dart';
+import 'package:parts_of_speech_quiz/screens/history/historyscreen.dart';
+import 'package:parts_of_speech_quiz/screens/homescreen.dart';
+import 'package:parts_of_speech_quiz/screens/quiz/quizmenu.dart';
+import 'package:parts_of_speech_quiz/screens/quiz/quizscreen.dart';
+import 'package:parts_of_speech_quiz/screens/study/studymenu.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:intl/intl.dart';
 import 'package:parts_of_speech_quiz/screens/history/historymodel.dart';
@@ -12,12 +18,11 @@ class ScoreScreen extends StatefulWidget {
   final String category;
   final String difficulty;
 
-  ScoreScreen({
-    required this.score,
-    required this.totalQuestions,
-    required this.category,
-    required this.difficulty
-  });
+  ScoreScreen(
+      {required this.score,
+      required this.totalQuestions,
+      required this.category,
+      required this.difficulty});
 
   @override
   State<ScoreScreen> createState() => _ScoreScreenState();
@@ -38,27 +43,27 @@ class _ScoreScreenState extends State<ScoreScreen> {
     List<String>? listString = loadpref?.getStringList(widget.category);
     // print(widget.category);
     // print(listString);
-    if(listString != null){
-      list = listString.map(
-              (item) => History.fromMap(json.decode(item))
-      ).toList();
-      setState((){});
+    if (listString != null) {
+      list =
+          listString.map((item) => History.fromMap(json.decode(item))).toList();
+      setState(() {});
     }
     saveData();
   }
 
-  void saveData() async{
+  void saveData() async {
     final sharedPreferences = await SharedPreferences.getInstance();
-    list.insert(0,
-        History(category: widget.category,
-                difficulty: widget.difficulty,
-                score: "${widget.score}/${widget.totalQuestions}",
-                date: date,
-                time: time,
+    list.insert(
+        0,
+        History(
+          category: widget.category,
+          difficulty: widget.difficulty,
+          score: "${widget.score}/${widget.totalQuestions}",
+          date: date,
+          time: time,
         ));
-    List<String> stringList = list.map(
-            (item) => json.encode(item.toMap()
-        )).toList();
+    List<String> stringList =
+        list.map((item) => json.encode(item.toMap())).toList();
     // print(widget.category);
     sharedPreferences.setStringList(widget.category, stringList);
     // print(stringList);
@@ -73,17 +78,146 @@ class _ScoreScreenState extends State<ScoreScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-          child: Column(
-            children: <Widget>[
-              Text('Score Screen', style: TextStyle( fontSize: 45.0, fontFamily: appFont, fontWeight: FontWeight.bold )),
-              Text('Category: ${widget.category}', style: TextStyle( fontSize: 45.0, fontFamily: appFont, fontWeight: FontWeight.bold )),
-              Text('Difficulty: ${widget.difficulty}', style: TextStyle( fontSize: 45.0, fontFamily: appFont, fontWeight: FontWeight.bold )),
-              Text('Score: ${widget.score}/ ${widget.totalQuestions}', style: TextStyle( fontSize: 45.0, fontFamily: appFont, fontWeight: FontWeight.bold )),
-
+      body: Stack(
+        fit: StackFit.expand,
+        children: [
+          Image.asset('assets/images/bg-1.png', fit: BoxFit.fill),
+          Column(children: [
+            Spacer(),
+            if (widget.score >= widget.totalQuestions * 0.75) ...[
+              Text(
+                "Passed!",
+                style: TextStyle(
+                    height: 1.6,
+                    fontSize: 130.0,
+                    fontFamily: appFont,
+                    color: Colors.indigo.shade600,
+                    fontWeight: FontWeight.bold),
+              )
+            ] else ...[
+              Text(
+                "Failed!",
+                style: TextStyle(
+                    height: 1.6,
+                    fontSize: 130.0,
+                    fontFamily: appFont,
+                    color: Colors.red,
+                    fontWeight: FontWeight.bold),
+              )
             ],
-          )
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text('${widget.score}',
+                    style: TextStyle(
+                        height: 0.2,
+                        fontSize: 100.0,
+                        fontFamily: appFont,
+                        fontWeight: FontWeight.bold)),
+                Text(' out of ${widget.totalQuestions}',
+                    style: TextStyle(
+                        height: 0.2,
+                        fontSize: 50.0,
+                        fontFamily: appFont,
+                        fontWeight: FontWeight.bold)),
+              ],
+            ),
+            Text('${widget.category} - ${widget.difficulty} Round',
+                style: TextStyle(
+                    fontSize: 45.0,
+                    fontFamily: appFont,
+                    fontWeight: FontWeight.bold)),
+            Spacer(),
+            if (widget.score >= widget.totalQuestions * 0.75) ...[
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  elevatedButton(
+                      text: "Next",
+                      screen: QuizMenu(),
+                      context: context,
+                      width: 180,
+                      gradient: blueGradientCen),
+                  elevatedButton(
+                      text: "Scores",
+                      screen: HistoryScreen(),
+                      context: context,
+                      width: 180,
+                      gradient: blueGradientCen),
+                ],
+              ),
+            ] else ...[
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  elevatedButton(
+                      text: "Study",
+                      screen: StudyMenu(),
+                      context: context,
+                      width: 180,
+                      gradient: blueGradientCen),
+                  elevatedButton(
+                      text: "Scores",
+                      screen: HistoryScreen(),
+                      context: context,
+                      width: 180,
+                      gradient: blueGradientCen)
+                ],
+              )
+            ],
+            elevatedButton(
+                text: "Main Menu",
+                screen: HomeScreen(),
+                context: context,
+                width: 380,
+                gradient: blueGradientCen),
+            Spacer(),
+          ]),
+        ],
       ),
     );
   }
+}
+
+Material elevatedButton(
+    {required text,
+    required screen,
+    required BuildContext context,
+    required double width,
+    required gradient}) {
+  return Material(
+    color: Colors.transparent,
+    shape: StadiumBorder(),
+    elevation: 10,
+    child: Container(
+      width: width,
+      decoration: BoxDecoration(
+        gradient: gradient,
+        borderRadius: BorderRadius.all(Radius.circular(30)),
+      ),
+      margin: EdgeInsets.all(6),
+      child: ElevatedButton(
+        onPressed: () {
+          Navigator.push(
+              context, MaterialPageRoute(builder: (context) => screen));
+        },
+        child: Container(
+            child: Text(text,
+                style: TextStyle(
+                    fontSize: 45.0,
+                    letterSpacing: 3,
+                    fontFamily: appFont,
+                    fontWeight: FontWeight.bold))),
+        style: ElevatedButton.styleFrom(
+          primary: Colors.transparent,
+          onSurface: Colors.transparent,
+          shadowColor: Colors.transparent,
+          elevation: 0,
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
+          //make color or elevated button transparent
+        ),
+      ),
+    ),
+  );
 }
--- lib/screens/study/studymenu.dart
+++ lib/screens/study/studymenu.dart
@@ -19,7 +19,10 @@ class StudyMenu extends StatelessWidget {
           leading: IconButton(
               icon: Icon(Icons.arrow_back_ios),
               onPressed: () {
-                Navigator.pop(context);
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: ((context) => const HomeScreen())));
               }),
           flexibleSpace: Container(
             decoration: BoxDecoration(gradient: blueGradientCen),
@@ -36,7 +39,11 @@ class StudyMenu extends StatelessWidget {
                     ),
                     gradient: blueGradientCen,
                     boxShadow: [
-                      new BoxShadow(color: Colors.black.withOpacity(0.5), offset: new Offset(-3.0, 3.0), blurRadius: 2.0, spreadRadius: 2.0)
+                      new BoxShadow(
+                          color: Colors.black.withOpacity(0.5),
+                          offset: new Offset(-3.0, 3.0),
+                          blurRadius: 2.0,
+                          spreadRadius: 2.0)
                     ]),
                 child: Stack(children: [
                   Positioned(
@@ -52,7 +59,16 @@ class StudyMenu extends StatelessWidget {
                               topRight: Radius.circular(15),
                             )),
                       )),
-                  Positioned(top: 0, bottom: 20, left: 30, child: Text("PART OF SPEECH", style: TextStyle(fontSize: 75.0, fontFamily: 'Dongle', fontWeight: FontWeight.bold, color: Colors.blueAccent.shade400)))
+                  Positioned(
+                      top: 0,
+                      bottom: 20,
+                      left: 30,
+                      child: Text("PART OF SPEECH",
+                          style: TextStyle(
+                              fontSize: 75.0,
+                              fontFamily: 'Dongle',
+                              fontWeight: FontWeight.bold,
+                              color: Colors.blueAccent.shade400)))
                 ])),
             SizedBox(
               height: height * 0.03,
@@ -62,28 +78,30 @@ class StudyMenu extends StatelessWidget {
               width: width * 0.9,
               child: Stack(
                 children: [
-                StyledText(
-                    text: "There are eight(8) parts of speech in the English language: <bold>noun</bold>, <bold>pronoun</bold>, <bold>verb</bold>, <bold>adjective</bold>, <bold>adverb</bold>, <bold>preposition</bold>, <bold>conjunction</bold>, and <bold>interjection</bold>. "
+                  StyledText(
+                    text:
+                        "There are eight(8) parts of speech in the English language: <bold>noun</bold>, <bold>pronoun</bold>, <bold>verb</bold>, <bold>adjective</bold>, <bold>adverb</bold>, <bold>preposition</bold>, <bold>conjunction</bold>, and <bold>interjection</bold>. "
                         "The part of speech indicates how the word functions in meaning as well as grammatically within the sentence. An individual word can function "
                         "as more than one part of speech when used in different circumstances. Understanding parts of speech is essential for determining the correct "
                         "definition of a word when using the dictionary.",
                     style: TextStyle(
-                      fontFamily: 'Dongle',
-                      fontSize: 28,
+                        fontFamily: 'Dongle',
+                        fontSize: 28,
                         letterSpacing: 2.0,
-                      height: 1.0,
+                        height: 1.0,
                         color: Colors.grey.shade900),
-                     textAlign: TextAlign.justify,
+                    textAlign: TextAlign.justify,
                     tags: {
-                      'bold': StyledTextTag(style: TextStyle(fontWeight: FontWeight.bold)),
+                      'bold': StyledTextTag(
+                          style: TextStyle(fontWeight: FontWeight.bold)),
                     },
                   ),
                 ],
               ),
             ),
-        elevatedButton(text: 'Continue', screen: AppBar_examples(), context: context),
+            elevatedButton(
+                text: 'Continue', screen: AppBar_examples(), context: context),
           ],
         ));
   }
 }
-
